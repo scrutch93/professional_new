84aa9c46f5d41e15edbcb3b3f839642610083d8c
Steven Crutchfield <stevccru@icloud.com>
2021-11-24 07:54:17 -0500

    updated devicons

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,12 @@ import initSr from "./sr";
 import { addResume } from "./utils";
 import resume from "../assets/Steven Crutchfield Technical Resume.pdf";
 
+
 export default function initApp() {
   initSr();
   initTilt();
   // comment this if you don't want to attach your resume
   addResume(resume);
+  
+  
 }
